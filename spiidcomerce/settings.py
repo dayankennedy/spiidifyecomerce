f775815396160910b797454a674a668052825372
dayankennedy <dayankennedyspiid@gmail.com>
2022-05-15 22:47:29 +0300

    remove whitenoise

--- spiidcomerce/settings.py
+++ spiidcomerce/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,7 +126,6 @@ STATICFILES_DIRS=[
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'AssetFiles')
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # media AssetFiles
 MEDIA_URL='/Media/'
