91d543858694227728da46b47890328e35c9db9b
dayankennedy <dayankennedyspiid@gmail.com>
2022-05-16 16:12:04 +0300

    nile file

--- spiidcomerce/settings.py
+++ spiidcomerce/settings.py
@@ -144,8 +144,3 @@ MEDIA_ROOT=os.path.join(BASE_DIR, 'Media/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-
-# Configure Django App for Heroku.
-import django_on_heroku
-django_on_heroku.settings(locals())
