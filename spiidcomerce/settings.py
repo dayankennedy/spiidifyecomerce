1f7273abf56c3da55e7b236901b8ee5b8249537a
dayankennedy <dayankennedyspiid@gmail.com>
2022-05-16 12:17:28 +0300

    psycopg2

--- spiidcomerce/settings.py
+++ spiidcomerce/settings.py
@@ -120,20 +120,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # media AssetFiles
 MEDIA_URL='/Media/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR, 'Media/')
-
-
 
 
 
