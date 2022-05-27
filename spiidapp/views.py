ed7efbd4dfc1852e590610c99ac9e4e16f30e561
dayankennedy <dayankennedyspiid@gmail.com>
2022-05-27 20:56:05 +0300

    new changes

--- spiidapp/views.py
+++ spiidapp/views.py
@@ -4,7 +4,7 @@ from django.core.paginator import Paginator
 
 #Views
 def base(request):
-    return render(request, 'spiidapp/base.html' )
+    return render(request, 'base.html' )
 # HOME VIEW
 def home(request):
     product_objects=Products.objects.all()
@@ -14,19 +14,19 @@ def home(request):
         product_objects=Products.objects.filter(title__icontains=item_name)
         
     # pagination code
-        
-        paginator=Paginator(product_objects,20)
-        page=request.GET.get('page')
-        product_objects=paginator.get_page(page)
-        
-    return render(request, 'spiidapp/home.html',  {'product_objects':product_objects} )
+    
+    paginator=Paginator(product_objects,20)
+    page=request.GET.get('page')
+    product_objects=paginator.get_page(page)
+    
+    return render(request, 'home.html',  {'product_objects':product_objects} )
 
 # CHECKOUT VIEW
 def checkout(request):
-    return render(request, 'spiidapp/checkout.html' )
+    return render(request, 'checkout.html' )
 
 # PRODUCT VIEW
 def product(request):
     
     # product_object=Products.objects.get(id=id,)
-    return render(request, 'spiidapp/product.html')
+    return render(request, 'product.html')
