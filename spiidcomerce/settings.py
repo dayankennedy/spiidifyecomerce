436a156c552830a39951483d086ee3a078b21f27
dayankennedy <dayankennedyspiid@gmail.com>
2022-05-27 15:55:43 +0300

    new

--- spiidcomerce/settings.py
+++ spiidcomerce/settings.py
@@ -131,10 +131,10 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static/'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
