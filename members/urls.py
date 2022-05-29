2624d9e560f2435286ee4ea16c3384878f2c46bc
dayankennedy <dayankennedyspiid@gmail.com>
2022-05-29 16:15:56 +0300

    ko

--- members/urls.py
+++ members/urls.py
@@ -6,7 +6,7 @@ from  .import views
 urlpatterns = [
     path('members/',views.signup, name='signup'),
     path('', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('loginPage/', views.loginPage, name='login'),
+    path('logoutPage/', views.logoutPage, name='logout'),
 ]
 
--- members/views.py
+++ members/views.py
@@ -1,19 +1,16 @@
-from django.urls import reverse
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from .forms import createUserForm
-from django.views import generic
-from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from django.contrib import messages
 from.forms import createUserForm
 from.models import Profile
-# Create your views here.
 
+# signup form
 def signup(request):
-    
-    # signup form
     form = createUserForm(request.POST)
     if request.method == 'POST':
         if form.is_valid():
@@ -25,40 +22,23 @@ def signup(request):
         form = createUserForm()
     return render(request, 'signup.html', {'form': form})
 
-    # login form
-    
-    def loginPage(request):
+# login form
+def loginPage(request):
         if request.method == 'POST':
             username = request.POST.get('username')
             password = request.POST.get('password')
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('post')
-            else:
-                messages.info(request, 'username OR password is incorrect')
-                
-                return redirect("login")
-        return render(request, 'login.html')
+                return redirect('home')
+        else:
+            messages.info(request, 'username OR password is incorrect')
+            return redirect("login")
+        return render(request, 'loginPage.html')
     
-    
-    # logout views
-    if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('post')
-            else:
-                messages.info(request, 'username OR password is incorrect')
-                return redirect("login")
+# logout
 
-    
-    return render(request,'signup.html')
-
-
-
-def logout(request):
-    return render(request, 'logout.html' )
+def logoutPage(request):
+        logout(request)
+        return redirect('login')
 
