d0ef0e90391edf6179061e38544cd6cb6beffd10
dayankennedy <dayankennedyspiid@gmail.com>
2022-06-07 14:03:11 +0300

    staticfiles

--- spiidcomerce/settings.py
+++ spiidcomerce/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = [
     
     ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'asset')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
